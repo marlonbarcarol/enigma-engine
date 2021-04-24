b8e6edfb5e9d6c6a2e17ade75300583ceb955392
Marlon <marlon.barcarol@gmail.com>
2021-04-24 18:02:32 +0100

    Finishing updating deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'prettier',
-		'prettier/@typescript-eslint',
 	],
 	rules: {
 		// Code analysis
