e9fef15b49ca7077c442e8b56fa6f292498f6df4
Marlon <marlon.barcarol@gmail.com>
2021-04-26 22:59:00 +0100

    Re-exporting

--- src/Configuration/index.ts
+++ src/Configuration/index.ts
@@ -2,7 +2,20 @@ import { Alphabet } from '@/Configuration/Alphabet/Alphabet';
 import { Plugboard } from '@/Configuration/Plugboard/Plugboard';
 import { Reflector } from '@/Configuration/Reflector/Reflector';
 import { Rotor } from '@/Configuration/Rotor/Rotor';
+import { RotorRing } from '@/Configuration/Rotor/RotorRing';
+import { RotorWiring } from '@/Configuration/Rotor/RotorWiring';
 import { Wheel } from '@/Configuration/Wheel/Wheel';
 import { Wiring } from '@/Configuration/Wiring/Wiring';
+import { EnigmaConfiguration } from '@/Configuration/EnigmaConfiguration';
 
-export { Alphabet, Plugboard, Reflector, Rotor, Wheel, Wiring };
+export {
+	Alphabet,
+	Plugboard,
+	Reflector,
+	Rotor,
+	RotorRing,
+	RotorWiring,
+	Wheel,
+	Wiring,
+	EnigmaConfiguration,
+};
--- src/EnigmaCipher.spec.ts
+++ src/EnigmaCipher.spec.ts
@@ -1,12 +1,15 @@
-import { Alphabet } from '@/Configuration/Alphabet/Alphabet';
-import { EnigmaConfiguration } from '@/Configuration/EnigmaConfiguration';
-import { Plugboard } from '@/Configuration/Plugboard/Plugboard';
-import { Reflector } from '@/Configuration/Reflector/Reflector';
-import { Rotor } from '@/Configuration/Rotor/Rotor';
-import { RotorRing } from '@/Configuration/Rotor/RotorRing';
-import { RotorWiring } from '@/Configuration/Rotor/RotorWiring';
-import { Wheel } from '@/Configuration/Wheel/Wheel';
-import { Wiring } from '@/Configuration/Wiring/Wiring';
+import {
+	Alphabet,
+	Plugboard,
+	Reflector,
+	Rotor,
+	RotorRing,
+	RotorWiring,
+	Wheel,
+	Wiring,
+	EnigmaConfiguration
+} from '@/Configuration';
+
 import { EnigmaCipher } from '@/EnigmaCipher';
 import { InvalidEnigmaAlphabetError } from '@/Error/InvalidEnigmaAlphabetError';
 
